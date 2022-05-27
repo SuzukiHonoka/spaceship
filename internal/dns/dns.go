5234088bc94854c4db5710e2f46bd2fb35a9f885
Suzukihonoka <sakurafirex@gmail.com>
2022-05-27 21:20:19 +0800

    dns: add SetDefault() function
    * Resolver property name changed to Server for avoid conflict

--- internal/dns/dns.go
+++ internal/dns/dns.go
@@ -1,6 +1,28 @@
 package dns
 
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+)
+
 type DNS struct {
 	Type
-	Resolver string
+	Server string
+}
+
+func (s *DNS) SetDefault() {
+	if s.Type != TypeCommon {
+		panic("not implemented")
+	}
+	net.DefaultResolver = &net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+			d := net.Dialer{
+				Timeout: 5 * time.Second,
+			}
+			return d.DialContext(ctx, network, fmt.Sprintf("%s:53", s.Server))
+		},
+	}
 }
