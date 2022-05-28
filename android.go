a83cc5e897a484ccc9c87600d07ea769bcb6a3a2
Suzukihonoka <sakurafirex@gmail.com>
2022-05-28 11:02:41 +0800

    bug: fix initial pointer

--- android.go
+++ android.go
@@ -8,7 +8,7 @@ import "spaceship/internal/dns"
 // android may not resolve dns correctly through DefaultResolver
 func init() {
 	// use dns server of dnspod by default
-	d := dns.DNS{
+	d := &dns.DNS{
 		Type:   dns.TypeCommon,
 		Server: "119.29.29.29",
 	}
