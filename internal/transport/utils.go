819156e453fa9555309bcbf51c97be15e6e4599c
Suzukihonoka <sakurafirex@gmail.com>
2022-05-30 21:41:15 +0800

    transport: add PrintErrorIfNotEOF to utils

--- internal/transport/utils.go
+++ internal/transport/utils.go
@@ -1,12 +1,21 @@
 package transport
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net"
 )
 
+func PrintErrorIfNotEOF(err error, msg string) {
+	if !errors.Is(err, io.EOF) && !errors.Is(err, net.ErrClosed) {
+		log.Printf("%s: %v", msg, err)
+	}
+}
+
 // GetTargetDst return dst addr and assume parameter won't be nil
-func GetTargetDst(fqdn string, port uint16) string {
+func GetTargetDst(fqdn string, port int) string {
 	var target string
 	ip := net.ParseIP(fqdn)
 	switch {
