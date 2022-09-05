f214a59a0355147806d1982ae072cf12e528a196
Suzukihonoka <sakurafirex@gmail.com>
2022-09-06 01:18:49 +0800

    logger: apply optimized style

--- internal/logger/mode.go
+++ internal/logger/mode.go
@@ -9,25 +9,24 @@ import (
 type Mode string
 
 const (
+	ModeDefault Mode = ""
 	ModeDiscard Mode = "null"
 )
 
 func (m Mode) Set() {
 	switch m {
+	case ModeDefault:
+		log.Println("log will be redirected to stdout")
+		log.SetOutput(os.Stdout)
 	case ModeDiscard:
 		log.Println("log disabled")
 		log.SetOutput(io.Discard)
 	default:
-		if m == "" {
-			log.Println("log will be redirected to stdout")
-			log.SetOutput(os.Stdout)
-		} else {
-			fd, err := os.OpenFile(string(m), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-			if err != nil {
-				log.Fatalf("Error when opening logfile: %s", err.Error())
-			}
-			log.Printf("log will be saved to %s", m)
-			log.SetOutput(fd)
+		fd, err := os.OpenFile(string(m), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			log.Fatalf("Error when opening logfile: %s", err.Error())
 		}
+		log.Printf("log will be saved to %s", m)
+		log.SetOutput(fd)
 	}
 }
