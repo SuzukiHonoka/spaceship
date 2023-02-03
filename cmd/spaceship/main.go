48398ebc9b8273a79261adc675bdee0c14445e3e
Suzukihonoka <sakurafirex@gmail.com>
2023-02-03 15:52:05 +0800

    cmd: add spaceship cli

---
+++ cmd/spaceship/main.go
@@ -0,0 +1,18 @@
+package spaceship
+
+import (
+	"flag"
+	"fmt"
+	"spaceship/api"
+	"spaceship/internal/config/manifest"
+)
+
+func Main() {
+	// first prompt
+	fmt.Printf("spaceship v%s ", manifest.VersionCode)
+	fmt.Println("for personal use only, absolutely without any warranty, any kind of illegal intention by using this program are strongly forbidden.")
+	// load configuration
+	configPath := flag.String("c", "./config.json", "config path")
+	flag.Parse()
+	api.LaunchFromFile(*configPath)
+}
