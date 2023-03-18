db5bbdba9021707c557fa2290598dfbdd8164cc0
Suzukihonoka <sakurafirex@gmail.com>
2023-03-18 16:13:25 +0800

    transport.route: implement routes proxy

--- internal/transport/router/cache.go
+++ internal/transport/router/cache.go
@@ -1,8 +1,29 @@
 package router
 
 var (
-	RoutesCache Routes
+	routesCache Routes
 	table       = &syncedRoutesTable{
 		RoutesTable: make(RoutesTable),
 	}
 )
+
+func GetCount() int {
+	return len(routesCache)
+}
+
+func AddToFirstRoute(r *Route) {
+	rs := Routes{r}
+	routesCache = append(rs, routesCache...)
+}
+
+func AddToLastRoute(r *Route) {
+	routesCache = append(routesCache, r)
+}
+
+func GetRoutes() Routes {
+	return routesCache
+}
+
+func SetRoutes(r Routes) {
+	routesCache = r
+}
