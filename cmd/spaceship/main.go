78cb93d40a48671b992c8f4ddb5e4290e2bfdb7c
Suzukihonoka <sakurafirex@gmail.com>
2023-04-12 15:32:11 +0800

    spaceship.cli: optimize

--- cmd/spaceship/main.go
+++ cmd/spaceship/main.go
@@ -8,11 +8,15 @@ import (
 )
 
 func main() {
-	// first prompt
-	fmt.Printf("spaceship v%s ", manifest.VersionCode)
-	fmt.Println("for personal use only, absolutely without any warranty, any kind of illegal intention by using this program are strongly forbidden.")
 	// load configuration
 	configPath := flag.String("c", "./config.json", "config path")
+	showVersion := flag.Bool("v", false, "show spaceship version")
 	flag.Parse()
+	if *showVersion {
+		fmt.Printf("spaceship v%s\n", manifest.VersionCode)
+		return
+	}
+	// prompt
+	fmt.Printf("spaceship v%s, for personal use only, absolutely without any warranty, any kind of illegal intention by using this program are strongly forbidden.\n", manifest.VersionCode)
 	api.NewLauncher().LaunchFromFile(*configPath)
 }
