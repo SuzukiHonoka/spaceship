92fcef12e2a042504863607ff29b5c728ee59589
Suzukihonoka <sakurafirex@gmail.com>
2023-05-10 23:51:33 +0800

    transport: increase default buffer size from 32K to 64K

--- internal/transport/params.go
+++ internal/transport/params.go
@@ -1,7 +1,7 @@
 package transport
 
-// BufferSize 32K (1K == 1024 Byte)
-var BufferSize = 32 * 1024
+// BufferSize 64K (1K == 1024 Byte)
+var BufferSize = 64 * 1024
 
 // Network is a tcp dial option
 var Network = "tcp"
